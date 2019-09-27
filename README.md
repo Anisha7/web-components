83c2245ad70cc650b05860fb0878a27363d6e114
Anisha Jain <anishajain@outlook.com>
2019-09-26 22:53:00 -0700

    blink

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # web-components
  Learning web components
+
+- Create a blink component. Everyone is lamenting the fact the blink tag has been removed.
+- Your blink component should take an attribute that defines the text it displays.
+- Use setInterval() to make the text "blink". You'll need to add an interval when the - Component is created and remove when the component is removed from the DOM. Use the lifecycle methods for this.
+- Use connectedCallback()
+- Use disconnectedCallback() to clear your interval
+- Use an attribute to set the time between blinks
+- Use two properties, one to set the on-time and another for the off-time.
--- blink.js
+++ blink.js
@@ -1,13 +1,23 @@
 (function() {
     // Make a new Component
     // Choose an element to extend, usually HTMLElement
-    class webComponentBase extends HTMLElement {
+    class Blink extends HTMLElement {
       constructor() {
         super(); // MUST call super!
         // Attach a shadow root to the element.
         this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._span = document.createElement('span');
+        this._textBody = this.innerHTML;
+        this._shadowRoot.appendChild(this._span)
+        this._span.innerHTML = this._textBody;
+        this._span.style.transition = "1000ms";
+        this._span.style.fontSize = "48px";
+        this._visibility = 1;
+        setInterval(() => {
+            this._visibility = this._visibility === 1 ? 0 : 1;
+            this._span.style.opacity = this._visibility;
+        }, 3000)
       }
-  
       // Defines the attributes accessible to JS
       static get observedAttributes() {
         return [] // List an array of attribute names
@@ -25,10 +35,18 @@
       // Lifecycle method called when this component is appeded to the DOM
       connectedCallback() {
         // Do things when component is added to the DOM
+        // Use setInterval() to make the text "blink". 
+        // You'll need to add an interval when the - Component is 
+        // created and remove when the component is removed from the DOM.
+        // setInterval()
+      }
+
+      render() {
+        
       }
     }
   
-    customElements.define('hello-world', webComponentBase);
+    customElements.define('blink-blink', Blink);
     // ---------
   
   
